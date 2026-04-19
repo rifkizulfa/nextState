c98d40574b2dc17e016a7c161ce9e21534cec935
rifki <rifkigalvatron202@gmail.com>
2026-04-19 20:12:06 +0700

    fix composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'taskflow/proxy',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0f0a42c803148a25982b52562afa9ec483514919',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'taskflow/proxy' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0f0a42c803148a25982b52562afa9ec483514919',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
