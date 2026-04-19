a5ad25228184dc64b238ce99a98beaab3e651c7c
rifki <rifkigalvatron202@gmail.com>
2026-04-19 20:20:07 +0700

    fix yoooo

--- api/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-// Forward Vercel requests to Laravel index.php in BE/laravel/public
-require __DIR__ . '/../BE/laravel/public/index.php';
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+// Forward Vercel requests to Laravel index.php
+require __DIR__ . '/BE/laravel/public/index.php';
