11f3ceff570f54d80152196c363434004b65f554
rifki <rifkigalvatron202@gmail.com>
2026-04-19 19:23:08 +0700

    fix ini mah

--- api/index.php
+++ api/index.php
@@ -1,4 +1,4 @@
 <?php
 
 // Forward Vercel requests to Laravel index.php in BE/laravel/public
-require __DIR__ . '/BE/laravel/public/index.php';
+require __DIR__ . '/../BE/laravel/public/index.php';
